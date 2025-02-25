13de5d3429856f9b54818dbe29e88a3119dcf6cb
Thiago-vini-gr <Thiagaogamerrj@gmail.com>
2025-02-25 11:23:45 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    class progam
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int dif = a - b;
+
+        Console.WriteLine($"A diferença é: {dif}");
+    }
+}
